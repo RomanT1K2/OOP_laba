816da744a258eecb6e6f39ac4ef5d4b3659a0171
RomanT1K2 <roman2212c@gmail.com>
2026-03-31 18:59:55 +0300

    зміна класа Main

--- Main.cpp
+++ Main.cpp
@@ -2,40 +2,29 @@
 #include "Menu.h"
 #include "Order.h"
 #include "Customer.h"
-#include "лаба_2.3.h"
 
 int main() {
+
+    MainCourse pizza("Margherita Pizza", 150, 800);
+    Salad caesar("Caesar Salad", 100, 200);
+    Salad greek("Greek Salad", 90, 180);
+
+    Salad copySalad = caesar;
+    Salad movedSalad = std::move(greek);
+
     Menu menu;
-	setlocale(LC_ALL, "Ukrainian");
-    menu.addDish(Dish("Піцца Маргарита", 180.50, 850));
-    menu.addDish(Dish("Борщ Український", 95.00, 320));
-    menu.addDish(Dish("Вареники з м'ясом", 120.00, 480));
-    menu.addDish(Dish("Салат Цезар", 110.00, 250));
+    menu.addItem(&pizza);
+    menu.addItem(&caesar);
+
     menu.showMenu();
 
-    Customer roman("Роман", "+380991234567");
-    Order order1(1, roman);
+    Customer c("Roman");
+    Order order(c);
 
-    order1.addDish(Dish("Піцца Маргарита", 180.50, 850));
-    order1.addDish(Dish("Салат Цезар", 110.00, 250));
-    order1.showOrder();
+    order.addItem(&pizza);
+    order.addItem(&caesar);
 
-    Customer olena("Олена", "+380671112233");
-    Order order2(2, olena, 10.0);
-
-    order2.addDish(Dish("Борщ Український", 95.00, 320));
-    order2.addDish(Dish("Вареники з м'ясом", 120.00, 480));
-    order2.showOrder();
-
-    double testPrice = -10.0;
-
-    if (testPrice < 0) {
-        std::cout << "\nПомилка: ціна не може бути від'ємною!\n";
-    }
-    else {
-        Dish good("Тест", testPrice);
-        good.showInfo();
-    }
+    order.showOrder();
 
     return 0;
 }
